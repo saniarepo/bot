1fe631347de99e7b962a435f5077236bb5dc7707
Alexandr Kuzovkov <G:\Users\Alexander\AppData\Roaming\The Bat!>
2015-03-27 21:42:51 +0300

    part 3: add obfuskation

--- bot/Program.cs
+++ bot/Program.cs
@@ -17,7 +17,8 @@ namespace bot
         public static ProcessMemory procGameMemory = null;
         public static IntPtr argumentAddress1;
         public static IntPtr argumentAddress2;
-     
+        public static int RandomOffset;
+        private Object Locker = new Object();
 
         static void Main(string[] args)
         {
@@ -50,7 +51,11 @@ namespace bot
             }
             var dwThreadId = Process.GetProcessesByName(nameProcGame)[0].Threads[0].Id;
             var ThreadHandle = procGameMemory.OpenThr((uint)dwThreadId);
-            var HookAddress = procGameMemory.AllocateMemory(6000);
+
+            var rnd = new Random();
+            RandomOffset = rnd.Next(0, 60);
+            var HookAddress = procGameMemory.AllocateMemory((uint)(6000 + rnd.Next(1, 2000))) + RandomOffset;
+
             argumentAddress1 = procGameMemory.AllocateMemory(80);
             procGameMemory.WriteBytes(argumentAddress1, new byte[80]);
             argumentAddress2 = procGameMemory.AllocateMemory(bufferSize);
@@ -58,7 +63,7 @@ namespace bot
             var resultAddress = procGameMemory.AllocateMemory(4);
             procGameMemory.Write(resultAddress, 0);
 
-
+            /*asm код который внедряется в память wow процесса*/
             List<string> asmLine = new List<string> {
                                             "pushfd",
                                             "pushad",
@@ -93,17 +98,10 @@ namespace bot
                                             "popfd"
                                         };
 
-            List<string> asmLine2 = new List<string> {
-                                            "pushfd",
-                                            "pushad",
-                                            "nop",
-                                            "nop",
-                                            "popad",
-                                            "popfd"
-                                        };
+            
             procGameMemory.Asm = new ManagedFasm(procGameMemory.getHandle());
             procGameMemory.Asm.Clear();
-            foreach (var str in asmLine)
+            foreach (var str in ObfuscateAsm(asmLine))
             {
                 procGameMemory.Asm.AddLine(str);
             }
@@ -115,6 +113,8 @@ namespace bot
             procGameMemory.Asm.AddLine("jmp " + (Address + OpCodes.Length));
             procGameMemory.Asm.Inject((uint)(((uint)HookAddress + length) + OpCodes.Length));
             procGameMemory.Asm.Clear();
+            
+            /*вставка перехода в начало функции в DirectX на наш Hook*/
             procGameMemory.Asm.AddLine("jmp " + HookAddress);
             for (var k = 0; k <= ((OpCodes.Length - 5) - 1); k++)
             {
@@ -127,7 +127,8 @@ namespace bot
             
         }
 
-        public byte[] InjectAndExecute(IEnumerable<string> asm, bool returnValue = false, int returnLength = 0)
+        /*функция для внедрения и вызова asm кода для вызова функций (непотокобезопасная, без обфускации) wow*/
+        public byte[] InjectAndExecuteOld(IEnumerable<string> asm, bool returnValue = false, int returnLength = 0)
         {
             procGameMemory.Asm.Clear();
             foreach (var str in asm)
@@ -150,6 +151,83 @@ namespace bot
             procGameMemory.FreeMemory(dwAddress);
 
             return result;
+        }
+
+        /*функция для внедрения и вызова asm кода для вызова функций wow (потокобезопасная, с обфускацией)*/
+        public byte[] InjectAndExecute(IEnumerable<string> asm, bool returnValue = false, int returnLength = 0)
+        {
+            var rnd = new Random();
+            var offset = 0;
+            uint dwAddress;
+            uint randomValue;
+            
+            
+            lock (Locker)
+            {
+                offset = 0;
+                randomValue = (uint)rnd.Next(0, 60);
+                //Наша очередь может хранить 80/4 = 20 значений
+                while (procGameMemory.Read((int)(argumentAddress1 + offset)) != 0 || procGameMemory.Read((int)(argumentAddress2 + offset)) != 0)
+                {
+                    offset += 4;
+                    if (offset >= 80)
+                    {
+                        offset = 0;
+                    }
+                }
+                procGameMemory.Asm.Clear();
+                foreach (var str in asm)
+                {
+                    for (var i = rnd.Next(0, 3); i >= 1; i--)
+                    {
+                        procGameMemory.Asm.AddLine(GetFakeCommand());
+                    }
+                    procGameMemory.Asm.AddLine(str);
+                }
+                dwAddress = (uint)procGameMemory.AllocateMemory((uint)(procGameMemory.Asm.Assemble().Length + rnd.Next(60, 80))) + randomValue;
+                procGameMemory.Asm.Inject(dwAddress);
+                procGameMemory.Write((IntPtr)argumentAddress1, (int)(dwAddress + offset));
+            }
+            while (procGameMemory.Read((int)(argumentAddress1 + offset)) > 0)
+            {
+                Thread.Sleep(1);
+            }
+            byte[] result = new byte[0];
+            if (returnValue)
+            {
+                result = procGameMemory.ReadBytes((int)procGameMemory.Read((int)(argumentAddress2 + offset)), returnLength);
+            }
+            procGameMemory.Write(argumentAddress2 + offset, 0);
+            procGameMemory.FreeMemory((IntPtr)(dwAddress - randomValue));
+
+            return result;
+        }
+
+        /**функция выдающая строку со случайной фейковой asm командой**/
+        internal static string GetFakeCommand()
+        {
+            var list = new List<string> { "mov edx, edx", "mov edi, edi", "xchg ebp, ebp", "mov esp, esp", "xchg esp, esp", "xchg edx, edx", "mov edi, edi" };
+            var rnd = new Random();
+            int num = rnd.Next(0, list.Count - 1);
+            return list[num];
+        }
+        
+        /**функция для добавления в список asm команд фейковых команд**/
+        internal static IEnumerable<string> ObfuscateAsm(IList<string> asmLines)
+        {
+            var rnd = new Random();
+            for (var i = asmLines.Count - 1; i >= 0; i--)
+            {
+                for (var k = rnd.Next(1, 4); k >= 1; k--)
+                {
+                    asmLines.Insert(i, GetFakeCommand());
+                }
+            }
+            for (var j = rnd.Next(1, 4); j >= 1; j--)
+            {
+                asmLines.Add(GetFakeCommand());
+            }
+            return asmLines;
         }
 
         
